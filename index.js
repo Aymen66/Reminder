e503a2962c4305a80e7cdc2d8d036acd125a21d4
Aymen <111906131+Aymen66@users.noreply.github.com>
2022-12-16 22:12:58 +0000

    changes made

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
+let tabs = document.querySelectorAll('[data-tab-target]')
+let tabContents = document.querySelectorAll('[data-tab-content]')
+
+tabs.forEach(tab => {
+  tab.addEventListener('click', () => {
+    let target = document.querySelector(tab.dataset.tabTarget)
+    tabContents.forEach(tabContent => {
+      tabContent.classList.remove('active')
+    })
+    tabs.forEach(tab => {
+      tab.classList.remove('active')
+    })
+    tab.classList.add('active')
+    target.classList.add('active')
+  })
+})
+
 let toDo = e => {
   let formData = JSON.parse(localStorage.getItem("formData")) || [];
   formData.push({
